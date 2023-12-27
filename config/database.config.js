32b635151ffbbde7c962018092e212ca979c0306
raattthilal <muhammedhilal.a@gmail.com>
2023-12-27 19:43:53 +0530

    db

--- config/database.config.js
+++ config/database.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     development: {
         // url:'mongodb+srv://englearn:QH9IGCkLDjf5sHFm@cluster0.9sx55vx.mongodb.net/?retryWrites=true&w=majority'
-        url:'mongodb+srv://infoenglearn360:nPb4NXVL8a2lSZfg@letstalkenglish.lyq0jld.mongodb.net/?retryWrites=true&w=majority'
+        url:'mongodb+srv://infoenglearn360:nPb4NXVL8a2lSZfg@letstalkenglish.lyq0jld.mongodb.net/test?retryWrites=true&w=majority&tls=true'
     },
     production: {
-        url:'mongodb+srv://infoenglearn360:nPb4NXVL8a2lSZfg@letstalkenglish.lyq0jld.mongodb.net/?retryWrites=true&w=majority'
+        url:'mongodb+srv://infoenglearn360:nPb4NXVL8a2lSZfg@letstalkenglish.lyq0jld.mongodb.net/test?retryWrites=true&w=majority&tls=true'
     }
 }
