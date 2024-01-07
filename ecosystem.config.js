6d29b5d4acd3943566c0ed4d3aa67937f5bf076e
raattthilal <muhammedhilal.a@gmail.com>
2024-01-07 14:14:37 +0530

    script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: 'master.service.js',
     instances: 1,
     autorestart: true,
-    watch: true,
+    watch: false,
     max_memory_restart: '1G',
     env: {
         NODE_ENV: 'production',
