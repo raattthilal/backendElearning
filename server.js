662de9756654dc4f1df9865d77976a51a58bf35e
raattthilal <muhammedhilal.a@gmail.com>
2024-01-08 13:03:42 +0530

    path

--- server.js
+++ server.js
@@ -73,9 +73,9 @@ module.exports = {
             }
 
             // Read SSL/TLS certificates
-            const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.letstalkenglish.co.in/privkey.pem', 'utf8');
-            const certificate = fs.readFileSync('/etc/letsencrypt/live/www.letstalkenglish.co.in/fullchain.pem;', 'utf8');
-            const ca = fs.readFileSync('/etc/letsencrypt/live/www.letstalkenglish.com/chain.pem', 'utf8');
+            const privateKey = fs.readFileSync('www.letstalkenglish.co.in/privkey.pem', 'utf8');
+            const certificate = fs.readFileSync('www.letstalkenglish.co.in/fullchain.pem;', 'utf8');
+            const ca = fs.readFileSync('www.letstalkenglish.com/chain.pem', 'utf8');
 
             const credentials = { key: privateKey, cert: certificate, ca: ca };
 
