e5cc3e2a5009936f64a78fd95ef6492203491a22
raattthilal <muhammedhilal.a@gmail.com>
2024-01-08 12:57:26 +0530

    https

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
         NODE_ENV: 'production',
-        port: 5000
+        PORT: 5000
     },
   }],
 
--- server.js
+++ server.js
@@ -1,43 +1,48 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 
 const Controller = require('./base/controller.js');
 var consoleArguments = require('minimist');
 var argv = consoleArguments(process.argv.slice(2));
-//Db Connection
+
+// Db Connection
 var env = process.env.NODE_ENV;
-env = env ? env : "development";
-console.log("Environment is " + env);
+env = env ? env : 'development';
+console.log('Environment is ' + env);
 const params = require('./config/params.config');
 const dbConfig = require('./config/database.config.js')[env];
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-// create express app
+// Create express app
 const app = express();
 app.use(cors());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-// parse requests of content-type - application/json
+// Parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// Parse requests of content-type - application/json
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 module.exports = {
     connectToDb: function (callback) {
-
-        mongoose.connect(dbConfig.url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }).then(() => {
-            console.log("Successfully connected to the database");
-            if (callback) {
-                callback();
-            }
-        }).catch(err => {
-            console.log('Could not connect to the database. Exiting now...', err);
-            process.exit();
-        });
+        mongoose
+            .connect(dbConfig.url, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
+            .then(() => {
+                console.log('Successfully connected to the database');
+                if (callback) {
+                    callback();
+                }
+            })
+            .catch((err) => {
+                console.log('Could not connect to the database. Exiting now...', err);
+                process.exit();
+            });
     },
     methods: {
         loadController: function (controller) {
@@ -60,28 +65,33 @@ module.exports = {
     start: function (serviceName, routes) {
         var that = this;
         this.connectToDb(function () {
-
-            var port = process.env.port ? process.env.port : null;
+            var port = process.env.PORT ? process.env.PORT : null;
             port = port ? port : argv.port ? argv.port : null;
             if (!port) {
-                console.log("PORT not set for " + serviceName + " service");
-
+                console.log('PORT not set for ' + serviceName + ' service');
                 process.exit(0);
             }
+
+            // Read SSL/TLS certificates
+            const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.letstalkenglish.co.in/privkey.pem', 'utf8');
+            const certificate = fs.readFileSync('/etc/letsencrypt/live/www.letstalkenglish.co.in/fullchain.pem;', 'utf8');
+            const ca = fs.readFileSync('/etc/letsencrypt/live/www.letstalkenglish.com/chain.pem', 'utf8');
+
+            const credentials = { key: privateKey, cert: certificate, ca: ca };
+
+            // Create HTTPS server
+            const httpsServer = https.createServer(credentials, app);
+
             if (routes) {
-                var len = routes.length ? routes.length : 0;
-                var i = 0;
-                var route = null;
-                while (i < len) {
-                    route = routes[i];
-                    console.log("Loading route " + route);
+                routes.forEach((route) => {
+                    console.log('Loading route ' + route);
                     require('./app/routes/' + route + '.routes.js')(app, that.methods);
-                    i++;
-                }
-                app.listen(port, () => {
-                    console.log("Server is listening on port " + port);
+                });
+
+                httpsServer.listen(port, () => {
+                    console.log('Server is listening on port ' + port);
                 });
             }
         });
-    }
+    },
 };
